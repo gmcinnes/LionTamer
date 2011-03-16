b3adead31f81816e36a40fcba6930549f1d6c6ad
gmcinnes <grant.mcinnes@ehealthinnovation.org>
2011-03-16 17:00:40 -0400

    Only try and add passwords if we dont have one

--- cookbooks/homebrew/recipes/dbs.rb
+++ cookbooks/homebrew/recipes/dbs.rb
@@ -16,26 +16,24 @@ script "Setting mysql root user to have MDI throwaway password" do
     source ~/.cinderella.profile
     # Break early if a password has been set
     mysqladmin -u root password 2>&1 | grep 'Access denied'
-    if [ $? -eq 0 ]; then
-      exit 0
-    fi;
+    if [ $? -ne 0 ]; then
+      # Try a bunch of times to set a password - if mysql has just
+      # been restarted it might take it a while to come up
+      fail_counter=0
+      for (( ; ; ))
+      do
+        mysqladmin -u root password #{THROWAWAY_PASSWORD} 
+        if [ $? -ne 0 ]; then
+          fail_counter=$(($fail_counter + 1))
+          sleep 1
+        fi
 
-    # Try a bunch of times to set a password - if mysql has just
-    # been restarted it might take it a while to come up
-    fail_counter=0
-    for (( ; ; ))
-    do
-      mysqladmin -u root password #{THROWAWAY_PASSWORD} 
-      if [ $? -ne 0 ]; then
-        fail_counter=$(($fail_counter + 1))
-        sleep 1
-      fi
-
-      if [ $fail_counter -gt 60 ]; then
-        echo "Couldn't connect to mysql"
-        break
-      fi
-    done
+        if [ $fail_counter -gt 60 ]; then
+          echo "Couldn't connect to mysql"
+          break
+        fi
+      done
+    fi
   EOS
 end
 
