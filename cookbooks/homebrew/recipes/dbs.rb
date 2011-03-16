bb280bab663a2ca428f64ba9944a2617a78c432a
gmcinnes <grant.mcinnes@ehealthinnovation.org>
2011-03-16 17:36:51 -0400

    Try and check mysql more clearly

--- cookbooks/homebrew/recipes/dbs.rb
+++ cookbooks/homebrew/recipes/dbs.rb
@@ -14,26 +14,31 @@ script "Setting mysql root user to have MDI throwaway password" do
   interpreter "bash"
   code <<-EOS
     source ~/.cinderella.profile
-    # Break early if a password has been set
-    mysqladmin -u root password 2>&1 | grep 'Access denied'
-    if [ $? -ne 0 ]; then
-      # Try a bunch of times to set a password - if mysql has just
-      # been restarted it might take it a while to come up
-      fail_counter=0
-      for (( ; ; ))
-      do
-        mysqladmin -u root password #{THROWAWAY_PASSWORD} 
-        if [ $? -ne 0 ]; then
-          fail_counter=$(($fail_counter + 1))
-          sleep 1
-        fi
+    # Try a bunch of times to set a password - if mysql has just
+    # been restarted it might take it a while to come up
+    fail_counter=0
+    for (( ; ; ))
+    do
+      # Break early if a password has been set
+      mysqladmin -u root password 2>&1 | grep 'Access denied'
+      if [ $? -ne 0]; then
+        echo "Password already set"
+        break; 
+      fi
 
-        if [ $fail_counter -gt 60 ]; then
-          echo "Couldn't connect to mysql"
-          break
-        fi
-      done
-    fi
+      # Try and set the password. Increment fail counter if
+      # we can't set the pword
+      mysqladmin -u root password #{THROWAWAY_PASSWORD} 
+      if [ $? -ne 0 ]; then
+        fail_counter=$(($fail_counter + 1))
+      fi
+
+      if [ $fail_counter -gt 60 ]; then
+        echo "Couldn't connect to mysql"
+        break
+      fi
+      sleep 1 
+    done
   EOS
 end
 
