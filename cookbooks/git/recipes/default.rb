ba1593ff141d32327f102fc51f6150c0ea6bfbaa
Dave <grant.mcinnes@wer.as>
2011-03-17 11:58:46 -0400

    Dont ask for git params unless we need them

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -7,19 +7,87 @@ require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 require 'etc'
 
+module GitInfoHelpers
+
+  def get_parameter_from_gitconfig(name)
+    config = File.read("#{ENV['HOME']}/.gitconfig")
+    matches = config.match /^ *#{name} *= *(.*)$/
+    matches[1].strip rescue nil
+  end
+
+  def gitconfig_exists?
+    File.exist?("#{ENV['HOME']}/.gitconfig")
+  end
+
+  def populate_parameters
+    @__email = get_parameter_from_gitconfig('email')
+    @__name  = get_parameter_from_gitconfig('name')
+    @__user  = get_parameter_from_gitconfig('user')
+    @__token = get_parameter_from_gitconfig('token')
+    @__editor = get_parameter_from_gitconfig('editor')
+  end
+
+  def parameters_all_populated?
+    populate_parameters
+    @__email && @__name && @__user && @__token && @__editor
+  end
+
+  def wants_to_use_gitconfig?
+    puts "Use existing ~/.gitconfig variables for configuring git?"
+    puts "Your current variables are:"
+    puts "Email: #{@__email}"
+    puts "Full Name: #{@__name}"
+    puts "Github User Name: #{@__user}"
+    puts "Github API Token: #{@__token}"
+    puts "Editor: #{@__editor}"
+    puts ""
+    puts "(Y/N): "
+    choice = $stdin.gets.strip.upcase
+    choice == "Y" 
+  end
+
+
+  def collect_parameters
+    puts ""
+    puts "Enter your work email: "
+    @__email = $stdin.gets.strip
+    puts ""
+    puts "Enter your full name: "
+    @__name = $stdin.gets.strip
+    puts ""
+    puts "Enter your github user name: "
+    @__user = $stdin.gets.strip
+    puts ""
+    puts "Enter your github token: "
+    @__token = $stdin.gets.strip
+    puts ""
+    @__editor = "/usr/bin/vim"
+  end
+end
+
 template "#{ENV['HOME']}/.gitconfig" do
+  include GitInfoHelpers
+
   mode   0700
   owner  ENV['USER']
   group  Etc.getgrgid(Process.gid).name
   source "dot.gitconfig.erb"
+  if gitconfig_exists? && 
+     parameters_all_populated? && 
+     wants_to_use_gitconfig?
+     nil # We already have the parameters if we got this far 
+  else
+    collect_parameters 
+  end
+  
   variables({
     :home         => ENV['HOME'],
-    :user         => ENV['USER'],
-    :email        => ENV['EMAIL'],
-    :github_user  => ENV['GITHUB_USER'],
-    :github_token => ENV['GITHUB_TOKEN'],
-    :editor       => ENV['EDITOR']   || fail("No editor set for your ~/.gitconfig"),
-    :fullname     => ENV['FULLNAME'] || fail("No Full Name set for your ~/.gitconfig")
+    :user         => @__name,
+    :email        => @__email,
+    :github_user  => @__github_user,
+    :github_token => @__token,
+    :editor       => @__editor,
+    :fullname     => @__name
   })
 end
 
