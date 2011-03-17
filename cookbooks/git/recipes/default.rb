041ee5ebe5cf130602358307250edec18403874d
asdfasdf <foo@bar>
2011-03-17 12:33:50 -0400

    Better git configuration

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -33,15 +33,18 @@ module GitInfoHelpers
   end
 
   def wants_to_use_gitconfig?
+    puts ""
     puts "Use existing ~/.gitconfig variables for configuring git?"
+    puts ""
     puts "Your current variables are:"
+    puts ""
     puts "Email: #{@__email}"
     puts "Full Name: #{@__name}"
     puts "Github User Name: #{@__user}"
     puts "Github API Token: #{@__token}"
     puts "Editor: #{@__editor}"
     puts ""
-    puts "(Y/N): "
+    print "(Y/N): "
     choice = $stdin.gets.strip.upcase
     choice == "Y" 
   end
@@ -49,24 +52,29 @@ module GitInfoHelpers
 
   def collect_parameters
     puts ""
-    puts "Enter your work email: "
+    puts "Collecting parameters for git install"
+    puts ""
+    print "Enter your work email: "
     @__email = $stdin.gets.strip
     puts ""
-    puts "Enter your full name: "
+    print "Enter your full name: "
     @__name = $stdin.gets.strip
     puts ""
-    puts "Enter your github user name: "
+    print "Enter your github user name: "
     @__user = $stdin.gets.strip
     puts ""
-    puts "Enter your github token: "
+    print "Enter your github token: "
     @__token = $stdin.gets.strip
     puts ""
     @__editor = "/usr/bin/vim"
   end
 end
 
-template "#{ENV['HOME']}/.gitconfig" do
+class Chef::Resource::Template
   include GitInfoHelpers
+end
+
+template "#{ENV['HOME']}/.gitconfig" do
 
   mode   0700
   owner  ENV['USER']
@@ -84,7 +92,7 @@ template "#{ENV['HOME']}/.gitconfig" do
     :home         => ENV['HOME'],
     :user         => @__name,
     :email        => @__email,
-    :github_user  => @__github_user,
+    :github_user  => @__user,
     :github_token => @__token,
     :editor       => @__editor,
     :fullname     => @__name
