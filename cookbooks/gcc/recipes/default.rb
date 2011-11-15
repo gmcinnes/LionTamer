2f9f679bb65d4ca0a5f393cf5d19785e569079ff
Grant McInnes <grant.mcinnes@eyesopen.ca>
2011-11-14 20:05:06 -0500

    Update cookbooks/gcc/recipes/default.rb

--- cookbooks/gcc/recipes/default.rb
+++ cookbooks/gcc/recipes/default.rb
@@ -5,8 +5,8 @@
 script "download gcc"
   interpreter "bash"
   code <<-EOS
+    echo "Downloading and installing GCC.  This will take a while"
   curl -sSL https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg > /tmp/gcc.pkg
   installer -package /tmp/gcc.pkg -target /
   EOS
-  end
 end
