5aeab952c531d93bb612cc2aa0e1bb6fca46108d
Grant McInnes <grant.mcinnes@eyesopen.ca>
2011-11-16 19:06:37 -0500

    fix typo

--- cookbooks/gcc/recipes/default.rb
+++ cookbooks/gcc/recipes/default.rb
@@ -6,8 +6,8 @@
 script "download gcc" do
   interpreter "bash"
   puts "Downloading and installing GCC.  This will take a while"
-    curl -sSL https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg > /tmp/gcc.pkg
   code <<-EOS
+    curl -sSL https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg > /tmp/gcc.pkg
     sudo installer -package /tmp/gcc.pkg -target /
   EOS
 end
