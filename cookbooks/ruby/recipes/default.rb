6332d81d7a55b3718d4719650b09b38d0a8df6de
Grant McInnes <grant.mcinnes@eyesopen.ca>
2011-11-19 22:25:47 -0500

    updates

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -25,7 +25,7 @@ script "installing rvm" do
    fi
 
    bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
-    
+   echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile    
   EOS
 end
 
